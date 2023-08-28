dd5af567306c40a91f4ad178ebcffc86fd88e84a
Mind <nathapat.mind@gmail.com>
2023-08-28 16:01:35 +0700

    finishhh

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,39 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import { Button, Container, Divider, Group, Pagination, Rating, Text, Textarea, Title } from "@mantine/core";
 
 export default function FoodReviewPage() {
+  
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
+      <Title order={4} mt="sm">Your rating</Title>
+      <Rating defaultValue={0} size="lg"/>
+      <Textarea
+      placeholder="Do you enjoy eating?"
+      label="Your review"
+      mt="xs"
+      minRows="3"
+      />
+      <Button color="orange" mt="xs">Submit Review </Button>
+
+      <Divider my="sm" />
+      <Group position="center">
+        <Title align="center" order={4}>Elon Musk</Title>
+        <Rating defaultValue={5} size="sm"/>
+      </Group>
+      <Text c="dimmed" align="center">Best pizza in this world. I give you X score.</Text>
+      <Divider my="sm" />
+      <Group position="center">
+        <Title align="center" order={4}>Mark Zuck</Title>
+        <Rating defaultValue={4} size="sm"/>
+      </Group>
+      <Text c="dimmed" align="center">My favourite part is pepperoni</Text>
+
+      <Pagination total="20" color="orange" position="center" mt="md"/>
+
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Nathapat Nerangsi 650610758
       </Text>
     </Container>
   );
